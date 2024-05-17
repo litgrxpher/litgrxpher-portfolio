31706a17fb42f2fbb6556d4b6f82897358b3717a
Adharsh CS <84709739+litgrxpher@users.noreply.github.com>
2024-05-18 02:49:25 +0530

    changes

--- script.js
+++ script.js
@@ -8,10 +8,25 @@ function toggleMenu() {
 function changeTextAndColor() {
     var textElement = document.querySelector('.section__text__p2');
     if (textElement.textContent === "Electronics Engineer") {
-        textElement.textContent = "Creative Design / Editor";
+        textElement.textContent = "Jack Of All Trades";
         textElement.style.color = "red";
     } else {
         textElement.textContent = "Electronics Engineer";
         textElement.style.color = ""; 
     }
 }
+
+const logoImg = document.getElementById('logo-img');
+const defaultImageSrc = './assets/lx.png'; 
+
+
+const hoverImageSrc = './assets/litgrxpher.png'; 
+
+
+logoImg.addEventListener('mouseover', function() {
+    logoImg.src = hoverImageSrc; 
+});
+
+logoImg.addEventListener('mouseout', function() {
+    logoImg.src = defaultImageSrc; 
+});
